ea4ace01cce7eab266edd8c26e9405dadf0e850d
Ryszard <49765395+Ryszard-S@users.noreply.github.com>
2022-11-22 15:01:56 +0100

    change requirements and add allowed hosts

--- pricetracker/settings.py
+++ pricetracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG')))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://price-tracker-76kz.onrender.com/']
 
 # Application definition
 
