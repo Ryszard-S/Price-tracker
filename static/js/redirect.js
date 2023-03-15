e28fb5c789720ef58cbf14611c17a6ff8f78e229
Ryszard <49765395+Ryszard-S@users.noreply.github.com>
2023-03-15 20:27:20 +0100

    update and change style for barcode, update footer

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -1,13 +1,12 @@
 let elem = document.getElementById("time");
 
-const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arr = [0, 1, 2, 3, 4, 5];
 arr.forEach((item) => {
-  console.log(item, 9 - item);
   setTimeout(() => {
-    elem.textContent = (9 - item).toString();
+    elem.textContent = (5 - item).toString();
   }, 1000 * item);
 });
 
 setTimeout(function () {
   document.location.href = "/";
-}, 10000);
+}, 6000);
